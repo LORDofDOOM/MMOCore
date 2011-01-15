4e123faa0c7d6edc8ee1d6ff6a8a2f71f4e53192
LORDofDOOM <tb@bbz-int.de>
2011-01-15 07:31:32 +0100

    add .goname and .namego commands back to core

---
+++ addition/world_command_namego.sql
@@ -0,0 +1,3 @@
+INSERT INTO `command` (`name`, `security`, `help`) VALUES
+('goname',1,'Syntax: .goname [$charactername]\r\n\r\nTeleport to the given character. Either specify the character name or click on the character\'s portrait, e.g. when you are in a group. Character can be offline.'),
+('namego',1,'Syntax: .namego [$charactername]\r\n\r\nTeleport the given character to you. Character can be offline.');
