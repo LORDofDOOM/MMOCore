8fc3ed1316e0f7a8de59aec568c0910baf32207b
LORDofDOOM <tb@bbz-int.de>
2011-01-16 01:26:57 +0100

    Quest fix for Escape from Skettis - merge from AC

---
+++ addition/quests/world_questfix_escape_from_skettis.sql
@@ -0,0 +1,88 @@
+-- [10446-10447]The_Final_Code.sql
+UPDATE `gameobject_template` SET `ScriptName`='npc_mana_bomb' WHERE `entry`=184725;
+DELETE FROM `script_texts` WHERE `entry` IN (-1000472,-1000473,-1000474,-1000475,-1000476) AND `npc_entry`=20767; 
+INSERT INTO `script_texts` (`npc_entry`,`entry`,`content_default`,`content_loc1`,`content_loc2`,`content_loc3`,`content_loc4`,`content_loc5`,`content_loc6`,`content_loc7`,`content_loc8`,`sound`,`type`,`language`,`emote`,`comment`) VALUES
+(20767,-1000472,'1...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'mana bomb SAY_COUNT_1'),
+(20767,-1000473,'2...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'mana bomb SAY_COUNT_2'),
+(20767,-1000474,'3...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'mana bomb SAY_COUNT_3'),
+(20767,-1000475,'4...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'mana bomb SAY_COUNT_4'),
+(20767,-1000476,'5...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'mana bomb SAY_COUNT_5');
+
+-- [11085]Quest_Escape_From_Skettis.sql
+-- Fix Quest Soporte Escape from Skettis http://www.wowhead.com/quest=11085
+UPDATE `creature_template` SET `ScriptName`='npc_skyguard_prisoner' WHERE `entry`=23383;
+DELETE FROM `script_texts` WHERE `entry` IN (-1000716,-1000717,-1000718) AND `npc_entry`=23383; 
+INSERT INTO `script_texts` (`npc_entry`,`entry`,`content_default`,`content_loc1`,`content_loc2`,`content_loc3`,`content_loc4`,`content_loc5`,`content_loc6`,`content_loc7`,`content_loc8`,`sound`,`type`,`language`,`emote`,`comment`) VALUES
+(23383,-1000716,'Thanks for your help. Let\'s get out of here!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'SAY_START'),
+(23383,-1000717,'Let\'s keep moving. I don\'t like this place.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'SAY_DONT_LIKE'),
+(23383,-1000718,'Thanks again. Sergeant Doryn will be glad to hear he has one less scout to replace this week.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'SAY_COMPLETE');
+
+-- [10922]Quest_Digging_Through_Bones.sql
+-- Fix Quest Soporte http://www.wowhead.com/quest=10922 "Digging Through Bones"
+UPDATE `creature_template` SET `ScriptName`='npc_letoll' WHERE `entry`=22458;
+DELETE FROM `script_texts` WHERE `entry` BETWEEN -1000531 AND -1000511 AND `npc_entry`=22458; 
+INSERT INTO `script_texts` (`npc_entry`,`entry`,`content_default`,`content_loc1`,`content_loc2`,`content_loc3`,`content_loc4`,`content_loc5`,`content_loc6`,`content_loc7`,`content_loc8`,`sound`,`type`,`language`,`emote`,`comment`) VALUES
+(22458,-1000511,'Aright, listen up! Form a circle around me and move out!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_START'),
+(22458,-1000512,'Aright, $r, just keep us safe from harm while we work. We\'ll pay you when we return.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_KEEP_SAFE'),
+(22458,-1000513,'The dig site is just north of here.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_NORTH'),
+(22458,-1000514,'We\'re here! Start diggin\'!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_ARRIVE'),
+(22458,-1000515,'I think there\'s somethin\' buried here, beneath the sand!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_BURIED'),
+(22458,-1000516,'Almost got it!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_ALMOST'),
+(22458,-1000517,'By brann\'s brittle bananas! What is it!? It... It looks like a drum.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_DRUM'),
+(22458,-1000518,'Wow... a drum.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_DRUM_REPLY'),
+(22458,-1000519,'This discovery will surely rock the foundation of modern archaeology.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_DISCOVERY'),
+(22458,-1000520,'Yea, great. Can we leave now? This desert is giving me hives.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_DISCOVERY_REPLY'),
+(22458,-1000521,'Have ye gone mad? You expect me to leave behind a drum without first beatin\' on it? Not this son of Ironforge! No sir!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_NO_LEAVE'),
+(22458,-1000522,'This reminds me of that one time where you made us search Silithus for evidence of sand gnomes.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_NO_LEAVE_REPLY1'),
+(22458,-1000523,'Or that time when you told us that you\'d discovered the cure for the plague of the 20th century. What is that even? 20th century?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_NO_LEAVE_REPLY2'),
+(22458,-1000524,'I don\'t think it can top the one time where he told us that he\'d heard that Artha\'s "cousin\'s" skeleton was frozen beneath a glacier in Winterspring. I\'ll never forgive you for that one, Letoll. I mean honestly... Artha\'s cousin?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_NO_LEAVE_REPLY3'),
+(22458,-1000525,'I dunno. It can\'t possibly beat the time he tried to convince us that we\'re all actually a figment of some being\'s imagination and that they only use us for their own personal amusement. That went over well during dinner with the family.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_NO_LEAVE_REPLY4'),
+(22458,-1000526,'Shut yer yaps! I\'m gonna bang on this drum and that\'s that!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_SHUT'),
+(22458,-1000527,'Say, do you guys hear that?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_REPLY_HEAR'),
+(22458,-1000528,'IN YOUR FACE! I told you there was somethin\' here!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_IN_YOUR_FACE'),
+(22458,-1000529,'Don\'t just stand there! Help him out!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_HELP_HIM'),
+(22458,-1000530,'%s picks up the drum.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll EMOTE_LE_PICK_UP'),
+(22458,-1000531,'You\'ve been a tremendous help, $r! Let\'s get out of here before more of those things show up! I\'ll let Dwarfowitz know you did the job asked of ya\' admirably.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'letoll SAY_LE_THANKS');
+DELETE FROM `script_waypoint` WHERE `entry`=22458;
+INSERT INTO `script_waypoint` (`entry`, `pointid`, `location_x`, `location_y`, `location_z`, `waittime`, `point_comment`) 
+VALUES
+(22458,0,-3739.907959,5393.691895,-4.213,5000, 'SAY_LE_KEEP_SAFE'),
+(22458,1,-3733.334229,5389.243164,-5.331,0,''),
+(22458,2,-3728.771729,5385.649414,-3.704,0,''),
+(22458,3,-3717.267090,5379.179199,-4.400,0,''),
+(22458,4,-3705.626465,5379.261719,-7.711,0,''),
+(22458,5,-3688.279541,5379.716309,-9.400,0,''),
+(22458,6,-3649.186523,5389.111816,-11.917,0,''),
+(22458,7,-3612.791504,5392.812500,-13.655,0,''),
+(22458,8,-3574.865479,5412.704590,-16.543,0,''),
+(22458,9,-3564.438232,5422.615723,-16.104,0,''),
+(22458,10,-3553.387695,5444.732910,-12.184,2500,'arivve dig site SAY_LE_ARRIVE'),
+(22458,11,-3557.291016,5465.319336,-9.282,7500,'dig 1'),
+(22458,12,-3548.102051,5453.417969,-12.282,10000,'dig 2 SAY_LE_BURIED pause'),
+(22458,13,-3556.580322,5446.475098,-11.920,0,'start returning'),
+(22458,14,-3564.438232,5422.615723,-16.104,0,''),
+(22458,15,-3574.865479,5412.704590,-16.543,0,''),
+(22458,16,-3612.791504,5392.812500,-13.655,0,''),
+(22458,17,-3649.186523,5389.111816,-11.917,0,''),
+(22458,18,-3688.279541,5379.716309,-9.400,0,''),
+(22458,19,-3705.626465,5379.261719,-7.711,0,''),
+(22458,20,-3717.267090,5379.179199,-4.400,0,''),
+(22458,21,-3728.771729,5385.649414,-3.704,0,''),
+(22458,22,-3733.334229,5389.243164,-5.331,0,''),
+(22458,23,-3739.907959,5393.691895,-4.213,0,'');
+
+-- -------------------------
+-- Netherstorm FULL
+-- -------------------------
+
+-- [10427]Quest_Creatures_Of_The_Eco_Domes.sql
+-- Fix Quest Creatures of the Eco-Domes 10427
+UPDATE `creature_template` SET `ScriptName`='mob_Talbuk' WHERE entry IN (20610,20777);
+
+-- [10409]Quest_Deathblow_to_the_Legion.sql
+-- Fix Quest Deathblow to the Legion http://www.wowhead.com/quest=10409
+UPDATE `quest_template` SET `SpecialFlags` = 0, `ReqCreatureOrGOCount1` = 1, `ReqCreatureOrGOId1` = 20132 WHERE `entry` = 10409;
+
+-- Quest Bring Me The Egg! http://www.wowhead.com/quest=10111
+UPDATE `creature_template` SET `unit_flags` = 0, `flags_extra` = 0 WHERE `entry` = 19055;
+
--- src/server/scripts/Outland/terokkar_forest.cpp
+++ src/server/scripts/Outland/terokkar_forest.cpp
@@ -443,9 +443,6 @@ public:
 
 };
 
-
-
-
 /*######
 ## npc_isla_starmane
 ######*/
@@ -635,7 +632,6 @@ public:
 
 };
 
-
 /*########
 ####npc_akuno
 #####*/
@@ -703,6 +699,524 @@ public:
 
 };
 
+/*######
+## npc_skyguard_prisoner
+## Quest Soporte Escape from Skettis http://www.wowhead.com/quest=11085
+######*/
+
+enum SPrisoner
+{
+    SAY_START                     = -1000716,
+    SAY_DONT_LIKE                 = -1000717,
+    SAY_COMPLETE                  = -1000718,
+    GO_PRISONER_CAGE              = 185952,
+    QUEST_ESCAPE_FROM_SKETTIS     = 11085,
+};
+
+class npc_skyguard_prisoner : public CreatureScript
+{
+public:
+    npc_skyguard_prisoner(): CreatureScript ("npc_skyguard_prisoner") { }
+    
+    bool OnQuestAccept(Player* pPlayer, Creature* pCreature, Quest const * pQuest)
+    {
+        if (pQuest->GetQuestId() == QUEST_ESCAPE_FROM_SKETTIS)
+        {
+            if (GameObject* pGo = pCreature->FindNearestGameObject(GO_PRISONER_CAGE, 10.0f))
+                pGo->UseDoorOrButton();
+            
+            if (npc_skyguard_prisoner::npc_skyguard_prisonerAI* pEscortAI = CAST_AI(npc_skyguard_prisoner::npc_skyguard_prisonerAI, pCreature->AI()))
+                pEscortAI->StartEvent(pPlayer, pQuest);
+        }
+        return true;
+    }
+
+    CreatureAI* GetAI(Creature* pCreature) const
+    {
+        return new npc_skyguard_prisonerAI(pCreature);
+    }
+
+    struct npc_skyguard_prisonerAI : public npc_escortAI 
+    {
+
+        npc_skyguard_prisonerAI(Creature *c) : npc_escortAI(c) { }
+
+        void Reset() { }
+        
+        uint32 CalculateWaypointID()
+        {
+            switch (me->GetGUIDLow())
+            {
+            case 1189307:
+                return 1;
+                break;
+            case 1189309:
+                return 2;
+                break;
+            default:
+                return 3;
+                break;
+            }
+        }
+
+        void StartEvent(Player* pPlayer, const Quest* pQuest)
+        {
+            switch (CalculateWaypointID())
+            {
+            case 1:
+                AddWaypoint(0, -4108.25f, 3032.18f, 344.799f, 3000);
+                AddWaypoint(1, -4114.41f, 3036.73f, 344.039f);
+                AddWaypoint(2, -4126.41f, 3026.07f, 344.156f);
+                AddWaypoint(3, -4145.17f, 3029.69f, 337.423f);
+                AddWaypoint(4, -4173.69f, 3035.72f, 343.346f);
+                AddWaypoint(5, -4173.70f, 3047.37f, 343.888f);
+                AddWaypoint(6, -4183.47f, 3060.62f, 344.157f, 3000);
+                AddWaypoint(7, -4179.13f, 3090.20f, 323.971f, 30000);
+                Start(false, false, pPlayer->GetGUID(), pQuest);
+                break;
+            case 2:
+                AddWaypoint(0, -3718.81f, 3787.24f, 302.890f, 3000);
+                AddWaypoint(1, -3714.44f, 3780.35f, 302.075f);
+                AddWaypoint(2, -3698.33f, 3788.04f, 302.171f);
+                AddWaypoint(3, -3679.36f, 3780.25f, 295.077f);
+                AddWaypoint(4, -3654.82f, 3770.43f, 301.291f);
+                AddWaypoint(5, -3656.07f, 3757.31f, 301.985f);
+                AddWaypoint(6, -3648.83f, 3743.07f, 302.173f, 3000);
+                AddWaypoint(7, -3659.16f, 3714.94f, 281.576f, 30000);
+                Start(false, false, pPlayer->GetGUID(), pQuest);
+                break;
+            case 3:
+                AddWaypoint(0, -3671.51f, 3385.36f, 312.956f, 3000);
+                AddWaypoint(1, -3677.74f, 3379.05f, 312.136f);
+                AddWaypoint(2, -3667.52f, 3366.45f, 312.233f);
+                AddWaypoint(3, -3672.87f, 3343.52f, 304.994f);
+                AddWaypoint(4, -3679.35f, 3319.01f, 311.419f);
+                AddWaypoint(5, -3692.93f, 3318.69f, 312.081f);
+                AddWaypoint(6, -3704.08f, 3309.56f, 312.233f, 3000);
+                AddWaypoint(7, -3733.99f, 3315.77f, 292.093f, 30000);
+                Start(false, false, pPlayer->GetGUID(), pQuest);
+                break;
+            }
+            return;
+        }
+
+        void WaypointReached(uint32 uiPointId)
+        {
+            if (Player* pPlayer = GetPlayerForEscort())
+            {
+                switch (uiPointId)
+                {
+                case 0:
+                    DoScriptText(SAY_START, me);
+                    break;
+                case 6:
+                    DoScriptText(SAY_DONT_LIKE, me);
+                    break;
+                case 7:
+                    DoScriptText(SAY_COMPLETE, me);
+                    if (Player* pPlayer = GetPlayerForEscort())
+                        pPlayer->GroupEventHappens(QUEST_ESCAPE_FROM_SKETTIS, me);
+                    break;
+                }
+            }
+        }
+
+        void UpdateAI(const uint32 uiDiff)
+        {
+            npc_escortAI::UpdateAI(uiDiff);
+
+            if (!UpdateVictim())
+                return;
+
+            DoMeleeAttackIfReady();
+        }
+    };
+};
+
+/*######
+## npc_mana_bomb
+## http://www.wowhead.com/quest=10446 The Final Code (Alliance)
+## http://www.wowhead.com/quest=10447 The Final Code (Horde)
+######*/
+
+enum eManaBomb
+{
+    SAY_COUNT_1                 = -1000472,
+    SAY_COUNT_2                 = -1000473,
+    SAY_COUNT_3                 = -1000474,
+    SAY_COUNT_4                 = -1000475,
+    SAY_COUNT_5                 = -1000476,
+    SPELL_MANA_BOMB_LIGHTNING   = 37843,
+    SPELL_MANA_BOMB_EXPL        = 35513,
+    NPC_MANA_BOMB_EXPL_TRIGGER  = 20767,
+    NPC_MANA_BOMB_KILL_TRIGGER  = 21039
+};
+
+class npc_mana_bomb : public CreatureScript
+{
+public:
+    npc_mana_bomb() : CreatureScript("npc_mana_bomb") { }
+
+    bool OnGossipHello(Player* pPlayer, GameObject* pGo)
+    {
+        if (Creature* pCreature = GetClosestCreatureWithEntry(pGo, NPC_MANA_BOMB_EXPL_TRIGGER, INTERACTION_DISTANCE))
+        {
+            if (npc_mana_bombAI* pBombAI = dynamic_cast<npc_mana_bombAI*>(pCreature->AI()))
+                pBombAI->DoTrigger(pPlayer, pGo);
+        }
+
+        return true;
+    }
+
+    CreatureAI* GetAI(Creature* pCreature) const
+    {
+        return new npc_mana_bombAI (pCreature);
+    }
+
+    struct npc_mana_bombAI : public ScriptedAI
+    {
+        npc_mana_bombAI(Creature* pCreature) : ScriptedAI(pCreature) 
+        { 
+            Reset(); 
+        }
+
+        GameObject* pManaBomb;
+
+        bool m_bIsActivated;
+        uint32 m_uiEventTimer;
+        uint32 m_uiEventCounter;
+
+        void Reset()
+        {
+            pManaBomb = NULL;
+            m_bIsActivated = false;
+            m_uiEventTimer = 1000;
+            m_uiEventCounter = 0;
+        }
+
+        void DoTrigger(Player* pPlayer, GameObject* pGo)
+        {
+            if (m_bIsActivated)
+                return;
+
+            m_bIsActivated = true;
+
+            pPlayer->KilledMonsterCredit(NPC_MANA_BOMB_KILL_TRIGGER,me->GetGUID());
+
+            pManaBomb = pGo;
+        }
+
+        void UpdateAI(const uint32 uiDiff)
+        {
+            if (!m_bIsActivated)
+                return;
+
+            if (m_uiEventTimer < uiDiff)
+            {
+                m_uiEventTimer = 1000;
+
+                if (m_uiEventCounter < 10)
+                    me->CastSpell(me, SPELL_MANA_BOMB_LIGHTNING, false);
+
+                switch(m_uiEventCounter)
+                {
+                    case 5:
+                        if (pManaBomb)
+                            pManaBomb->SetGoState(GO_STATE_ACTIVE);
+
+                        DoScriptText(SAY_COUNT_1, me);
+                        break;
+                    case 6:
+                        DoScriptText(SAY_COUNT_2, me);
+                        break;
+                    case 7:
+                        DoScriptText(SAY_COUNT_3, me);
+                        break;
+                    case 8:
+                        DoScriptText(SAY_COUNT_4, me);
+                        break;
+                    case 9:
+                        DoScriptText(SAY_COUNT_5, me);
+                        break;
+                    case 10:
+                        me->CastSpell(me, SPELL_MANA_BOMB_EXPL, false);
+                        break;
+                    case 30:
+                        if (pManaBomb)
+                            pManaBomb->SetGoState(GO_STATE_READY);
+
+                        Reset();
+                        break;
+                }
+
+                ++m_uiEventCounter;
+            }
+            else
+                m_uiEventTimer -= uiDiff;
+        }
+    };
+};
+
+/*######
+## npc_letoll
+## http://www.wowhead.com/quest=10922 "Digging Through Bones"
+######*/
+
+enum NpcLetoll
+{
+    SAY_LE_START                    = -1000511,
+    SAY_LE_KEEP_SAFE                = -1000512,
+    SAY_LE_NORTH                    = -1000513,
+    SAY_LE_ARRIVE                   = -1000514,
+    SAY_LE_BURIED                   = -1000515,
+    SAY_LE_ALMOST                   = -1000516,
+    SAY_LE_DRUM                     = -1000517,
+    SAY_LE_DRUM_REPLY               = -1000518,
+    SAY_LE_DISCOVERY                = -1000519,
+    SAY_LE_DISCOVERY_REPLY          = -1000520,
+    SAY_LE_NO_LEAVE                 = -1000521,
+    SAY_LE_NO_LEAVE_REPLY1          = -1000522,
+    SAY_LE_NO_LEAVE_REPLY2          = -1000523,
+    SAY_LE_NO_LEAVE_REPLY3          = -1000524,
+    SAY_LE_NO_LEAVE_REPLY4          = -1000525,
+    SAY_LE_SHUT                     = -1000526,
+    SAY_LE_REPLY_HEAR               = -1000527,
+    SAY_LE_IN_YOUR_FACE             = -1000528,
+    SAY_LE_HELP_HIM                 = -1000529,
+    EMOTE_LE_PICK_UP                = -1000530,
+    SAY_LE_THANKS                   = -1000531,
+    QUEST_DIGGING_BONES             = 10922,
+    NPC_RESEARCHER                  = 22464,
+    NPC_BONE_SIFTER                 = 22466,
+    MAX_RESEARCHER                  = 4
+};
+
+class npc_letoll : public CreatureScript
+{
+public:
+    npc_letoll() : CreatureScript("npc_letoll") { }
+
+    bool OnQuestAccept(Player* pPlayer, Creature* pCreature, Quest const* pQuest)
+    {
+        if (pQuest->GetQuestId() == QUEST_DIGGING_BONES)
+        {
+            if (npc_letollAI* pEscortAI = dynamic_cast<npc_letollAI*>(pCreature->AI()))
+            {
+                DoScriptText(SAY_LE_START, pCreature);
+                pCreature->setFaction(FACTION_ESCORT_N_NEUTRAL_PASSIVE);
+
+                pEscortAI->Start(false, false, pPlayer->GetGUID(), pQuest, true);
+            }
+        }        
+        return true;
+    }
+
+    CreatureAI* GetAI(Creature* pCreature) const
+    {
+        return new npc_letollAI(pCreature);
+    }
+
+    struct npc_letollAI : public npc_escortAI
+    {
+        npc_letollAI(Creature* pCreature) : npc_escortAI(pCreature)
+        {
+            m_uiEventTimer = 5000;
+            m_uiEventCount = 0;
+            Reset();
+        }
+        
+        std::list<Creature*> m_lResearchersList;
+        uint32 m_uiEventTimer;
+        uint32 m_uiEventCount;
+
+        void Reset() {}
+
+        void SetFormation()
+        {
+            uint32 uiCount = 0;
+
+            for (std::list<Creature*>::iterator itr = m_lResearchersList.begin(); itr != m_lResearchersList.end(); ++itr)
+            {
+                float fAngle = uiCount < MAX_RESEARCHER ? M_PI/MAX_RESEARCHER - (uiCount*2*M_PI/MAX_RESEARCHER) : 0.0f;
+
+                if ((*itr)->isAlive() && !(*itr)->isInCombat())
+                    (*itr)->GetMotionMaster()->MoveFollow(me, 2.5f, fAngle);
+
+                ++uiCount;
+            }
+        }
+
+        Creature* GetAvailableResearcher(uint8 uiListNum)
+        {
+            if (!m_lResearchersList.empty())
+            {
+                uint8 uiNum = 1;
+
+                for (std::list<Creature*>::iterator itr = m_lResearchersList.begin(); itr != m_lResearchersList.end(); ++itr)
+                {
+                    if (uiListNum && uiListNum != uiNum)
+                    {
+                        ++uiNum;
+                        continue;
+                    }
+
+                    if ((*itr)->isAlive() && (*itr)->IsWithinDistInMap(me, 20.0f))
+                        return (*itr);
+                }
+            }
+            return NULL;
+        }
+
+        void JustStartedEscort()
+        {
+            m_uiEventTimer = 5000;
+            m_uiEventCount = 0;
+
+            m_lResearchersList.clear();
+
+            float x, y, z;
+            me->GetPosition(x, y, z);
+
+            CellPair pair(Trinity::ComputeCellPair(x, y));
+            Cell cell(pair);
+            cell.data.Part.reserved = ALL_DISTRICT;
+            cell.SetNoCreate();
+
+            Trinity::AllCreaturesOfEntryInRange check(me, NPC_RESEARCHER, 25);
+            Trinity::CreatureListSearcher<Trinity::AllCreaturesOfEntryInRange> searcher(me,m_lResearchersList, check);
+            TypeContainerVisitor<Trinity::CreatureListSearcher<Trinity::AllCreaturesOfEntryInRange>, GridTypeMapContainer> cSearcher(searcher);
+            cell.Visit(pair, cSearcher, *(me->GetMap()));
+
+            if (!m_lResearchersList.empty())
+                SetFormation();
+        }
+
+        void WaypointReached(uint32 uiPointId)
+        {
+            switch(uiPointId)
+            {
+                case 0:
+                    if (Player* pPlayer = GetPlayerForEscort())
+                        DoScriptText(SAY_LE_KEEP_SAFE, me, pPlayer);
+                    break;
+                case 1:
+                    DoScriptText(SAY_LE_NORTH, me);
+                    break;
+                case 10:
+                    DoScriptText(SAY_LE_ARRIVE, me);
+                    break;
+                case 12:
+                    DoScriptText(SAY_LE_BURIED, me);
+                    SetEscortPaused(true);
+                    break;
+                case 13:
+                    SetRun();
+                    break;
+            }
+        }
+
+        void Aggro(Unit* pWho)
+        {
+            if (pWho->isInCombat() && pWho->GetTypeId() == TYPEID_UNIT && pWho->GetEntry() == NPC_BONE_SIFTER)
+                DoScriptText(SAY_LE_HELP_HIM, me);
+        }
+
+        void JustSummoned(Creature* pSummoned)
+        {
+            Player* pPlayer = GetPlayerForEscort();
+
+            if (pPlayer && pPlayer->isAlive())
+                pSummoned->AI()->AttackStart(pPlayer);
+            else
+                pSummoned->AI()->AttackStart(me);
+        }
+
+        void UpdateEscortAI(const uint32 uiDiff)
+        {
+            if (!UpdateVictim())
+            {
+                if (HasEscortState(STATE_ESCORT_PAUSED))
+                {
+                    if (m_uiEventTimer < uiDiff)
+                    {
+                        m_uiEventTimer = 7000;
+
+                        switch(m_uiEventCount)
+                        {
+                            case 0:
+                                DoScriptText(SAY_LE_ALMOST, me);
+                                break;
+                            case 1:
+                                DoScriptText(SAY_LE_DRUM, me);
+                                break;
+                            case 2:
+                                if (Creature* pResearcher = GetAvailableResearcher(0))
+                                    DoScriptText(SAY_LE_DRUM_REPLY, pResearcher);
+                                break;
+                            case 3:
+                                DoScriptText(SAY_LE_DISCOVERY, me);
+                                break;
+                            case 4:
+                                if (Creature* pResearcher = GetAvailableResearcher(0))
+                                    DoScriptText(SAY_LE_DISCOVERY_REPLY, pResearcher);
+                                break;
+                            case 5:
+                                DoScriptText(SAY_LE_NO_LEAVE, me);
+                                break;
+                            case 6:
+                                if (Creature* pResearcher = GetAvailableResearcher(1))
+                                    DoScriptText(SAY_LE_NO_LEAVE_REPLY1, pResearcher);
+                                break;
+                            case 7:
+                                if (Creature* pResearcher = GetAvailableResearcher(2))
+                                    DoScriptText(SAY_LE_NO_LEAVE_REPLY2, pResearcher);
+                                break;
+                            case 8:
+                                if (Creature* pResearcher = GetAvailableResearcher(3))
+                                    DoScriptText(SAY_LE_NO_LEAVE_REPLY3, pResearcher);
+                                break;
+                            case 9:
+                                if (Creature* pResearcher = GetAvailableResearcher(4))
+                                    DoScriptText(SAY_LE_NO_LEAVE_REPLY4, pResearcher);
+                                break;
+                            case 10:
+                                DoScriptText(SAY_LE_SHUT, me);
+                                break;
+                            case 11:
+                                if (Creature* pResearcher = GetAvailableResearcher(0))
+                                    DoScriptText(SAY_LE_REPLY_HEAR, pResearcher);
+                                break;
+                            case 12:
+                                DoScriptText(SAY_LE_IN_YOUR_FACE, me);
+                                me->SummonCreature(NPC_BONE_SIFTER, 0.0f, 0.0f, 0.0f, 0.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+                                break;
+                            case 13:
+                                DoScriptText(EMOTE_LE_PICK_UP, me);
+
+                                if (Player* pPlayer = GetPlayerForEscort())
+                                {
+                                    DoScriptText(SAY_LE_THANKS, me, pPlayer);
+                                    pPlayer->GroupEventHappens(QUEST_DIGGING_BONES, me);
+                                }
+
+                                SetEscortPaused(false);
+                                break;
+                        }
+
+                        ++m_uiEventCount;
+                    }
+                    else
+                        m_uiEventTimer -= uiDiff;
+                }
+
+                return;
+            }
+
+            DoMeleeAttackIfReady();
+        }
+    };
+};
+
 void AddSC_terokkar_forest()
 {
     new mob_unkor_the_ruthless();
@@ -715,4 +1229,7 @@ void AddSC_terokkar_forest()
     new npc_skywing();
     new npc_slim();
     new npc_akuno();
+    new npc_skyguard_prisoner();
+    new npc_mana_bomb();
+    new npc_letoll();
 }
