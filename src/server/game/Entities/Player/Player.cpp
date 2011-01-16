5d351e8878c78792b22a8a40930796384a33f578
Machiavelli <machiavelli.trinity@gmail.com>
2011-01-16 19:23:17 +0800

    Core/General: Fix some comment style. //* to // *. Fixes certain code display errors on nano. Thanks to Aokromes for pointing out.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -22884,9 +22884,9 @@ bool ItemPosCount::isContainedIn(ItemPosCountVec const& vec) const
     return false;
 }
 
-//***********************************
-//-------------TRINITY---------------
-//***********************************
+// ***********************************
+// -------------TRINITY---------------
+// ***********************************
 
 void Player::StopCastingBindSight()
 {
