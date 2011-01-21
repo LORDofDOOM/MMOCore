fadf182f0e478e8d7c55d96f85335472b5ecc62b
LORDofDOOM <tb@bbz-int.de>
2011-01-21 10:48:02 +0100

    Typo Fix

--- src/server/shared/Database/Implementation/CharacterDatabase.h
+++ src/server/shared/Database/Implementation/CharacterDatabase.h
@@ -451,8 +451,8 @@ static const PreparedStatementTable CharacterDatabasePreparedStatements[] =
     // Auras
     {CHAR_DEL_AURA, "DELETE FROM character_aura WHERE guid = ?", CONNECTION_ASYNC},
     {CHAR_ADD_AURA, "INSERT INTO character_aura (guid,caster_guid,item_guid,spell,effect_mask,recalculate_mask,stackcount,amount0,amount1,amount2,base_amount0,base_amount1,base_amount2,maxduration,remaintime,remaincharges) "
-        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", CONNECTION_ASYNC}
-    {CHAR_DEL_GUILD_GUILDHOUSE, "DELETE FROM gh_guildadd WHERE guildId = ?", CONNECTION_ASYNC},
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", CONNECTION_ASYNC},
+    {CHAR_DEL_GUILD_GUILDHOUSE, "DELETE FROM gh_guildadd WHERE guildId = ?", CONNECTION_ASYNC}
 };
 
 #endif
