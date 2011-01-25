0abd0d4e7d54877148166771a00eac04ce92846d
LORDofDOOM <tb@bbz-int.de>
2011-01-25 23:11:15 +0100

    Fix 10 stacks dualspec/talent reset bug http://www.wowhead.com/spell=11129 Combustion

---
+++ addition/spells/world_11129_combustion.sql
@@ -0,0 +1,2 @@
+-- 10 stacks dualspec/talent reset bug http://www.wowhead.com/spell=11129 Combustion
+INSERT INTO spell_linked_spell VALUES (-11129, -28682, 0, 'ControlAura removed, remove stacking CritBuff too');
