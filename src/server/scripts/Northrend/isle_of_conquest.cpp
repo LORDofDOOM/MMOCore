e3c003dcc709cc447e058578ebfeddc2e8c66567
LORDofDOOM <tb@bbz-int.de>
2011-01-30 09:05:24 +0100

    Fix Exploit Bosses Isle Of Conquest (Offlike Spells pendiente sniffer) - THX jorda321

--- src/server/scripts/Northrend/isle_of_conquest.cpp
+++ src/server/scripts/Northrend/isle_of_conquest.cpp
@@ -67,7 +67,124 @@ class npc_four_car_garage : public CreatureScript
         }
 };
 
+/*#####
+## Bosses Isle Of Conquest (A/H)
+## NPC_HIGH_COMMANDER_HALFORD_WYRMBANE = 34924, // Alliance Boss
+## NPC_OVERLORD_AGMAR = 34922, // Horde Boss
+## Comment: AI Basico segun WOWHEAD, para una principal solucion del exploit de LOS del BG.
+##          Futuro Sniffed de blizzard por (Edward). para sonidos textos y reales spells.
+#####*/
+
+enum SpellsBosses
+{
+    SPELL_DAGGER_THROW_1    = 67280,
+    SPELL_DAGGER_THROW_2    = 67881,
+    SPELL_CRUSHING_LEAP     = 68506,
+    SPELL_MORTAL_STRIKE     = 39171,
+};
+
+class bosses_isle_of_conquest : public CreatureScript
+{
+public:
+    bosses_isle_of_conquest() : CreatureScript("bosses_isle_of_conquest") { }
+
+    struct bosses_isle_of_conquestAI : public ScriptedAI
+    {
+        bosses_isle_of_conquestAI(Creature *c) : ScriptedAI(c) { }
+
+        uint32 uiMortalStrikeTimer;
+        uint32 uiDaggerThrowTimer;
+        uint32 uiCrushingLeapTimer;
+        uint32 uiResetTimer;
+
+        void Reset()
+        {
+            uiMortalStrikeTimer = 8*IN_MILLISECONDS;
+            uiDaggerThrowTimer  = 2*IN_MILLISECONDS;
+            uiCrushingLeapTimer = 6*IN_MILLISECONDS;
+            uiResetTimer        = 5*IN_MILLISECONDS;
+        }
+        
+        void EnterCombat(Unit * who)
+        {
+            if(!me->IsWithinLOSInMap(who))// Siguie LOS
+            {
+                EnterEvadeMode();
+            }
+        }
+
+        void JustRespawned()
+        {
+            Reset();
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (!UpdateVictim())
+                return;
+
+            if (uiMortalStrikeTimer < diff)
+            {
+                DoCast(me->getVictim(), SPELL_MORTAL_STRIKE);
+                uiMortalStrikeTimer = urand(10*IN_MILLISECONDS,20*IN_MILLISECONDS);
+            }
+            else
+                uiMortalStrikeTimer -= diff;
+
+            if (uiDaggerThrowTimer < diff)
+            {
+                if (Unit* pTarget = SelectTarget(SELECT_TARGET_RANDOM))
+                    DoCast(pTarget, RAID_MODE(SPELL_DAGGER_THROW_1, SPELL_DAGGER_THROW_2));
+                uiDaggerThrowTimer = urand(7*IN_MILLISECONDS,12*IN_MILLISECONDS);
+            }
+            else
+                uiDaggerThrowTimer -= diff;
+
+            if (uiCrushingLeapTimer < diff)
+            {
+                if (Unit* pTarget = SelectTarget(SELECT_TARGET_RANDOM))
+                    DoCast(pTarget, SPELL_CRUSHING_LEAP);
+                uiCrushingLeapTimer = urand(12*IN_MILLISECONDS,16*IN_MILLISECONDS);
+            }
+            else
+                uiCrushingLeapTimer -= diff;
+            
+            // Chequea si la creatura no esta al exterior del edificio (Antibugers >3)
+            if (uiResetTimer < diff)
+            {
+                float x,y;
+                me->GetPosition(x,y);
+
+                if (me->GetEntry()== NPC_OVERLORD_AGMAR && (x > 1348 || x < 1283 || y < -800 || y > -730))
+                {
+                    me->Kill(me->getVictim());
+                    me->MonsterSay("Antibugers:$N, No querias bugear rqlo :)", LANG_UNIVERSAL, me->GetGUID());
+                    EnterEvadeMode();
+                }
+
+                if (me->GetEntry()== NPC_HIGH_COMMANDER_HALFORD_WYRMBANE && (x > 288 || x < 216 || y < -863 || y > -800))
+                {
+                    EnterEvadeMode();
+                    me->MonsterSay("Antibugers:$N, No querias bugear rqlo :)", LANG_UNIVERSAL, me->GetGUID());
+                    me->Kill(me->getVictim());
+                }
+                uiResetTimer = 200;
+            }
+            else   
+                uiResetTimer -= diff;
+
+            DoMeleeAttackIfReady();
+        }
+    };
+
+    CreatureAI *GetAI(Creature *creature) const
+    {
+        return new bosses_isle_of_conquestAI(creature);
+    }
+};
+
 void AddSC_isle_of_conquest()
 {
     new npc_four_car_garage();
+    new bosses_isle_of_conquest();
 }
