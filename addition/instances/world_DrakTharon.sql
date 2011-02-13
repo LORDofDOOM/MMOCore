52b57e8dfc70b7e2d7d198673078f20e669bd9ff
LORDofDOOM <tb@bbz-int.de>
2011-02-13 20:40:53 +0100

    Merge Drak'Tharon with AC

--- addition/instances/world_DrakTharon.sql
+++ addition/instances/world_DrakTharon.sql
@@ -1,32 +1,32 @@
 -- Drak'Tharon Heroic Loot Emblems
 UPDATE creature_loot_template SET item = 40752 WHERE entry IN (SELECT lootid FROM creature_template WHERE entry IN (31362,31350,31360,31349)) AND item = 47241;
 
-## Insert Trash Loot Blues
-DELETE FROM creature_loot_template WHERE entry IN (
-31339, -- Drakkari Guardian
-31347, -- Ghoul Tormentor
-31337, -- Drakkari Bat
-31357, -- Scourge Brute
-31363, -- Wretched Belcher
-31336, -- Darkweb Recluse
-31359, -- Scourge Reanimator
-31343, -- Drakkari Scytheclaw
-31355, -- Risen Drakkari Warrior
-31354, -- Risen Drakkari Soulmage
-31342, -- Risen Drakkari Handler
-31351, -- Risen Drakkari Bat Rider
-31345, -- Drakkari Shaman
-31340, -- Drakkari Gutripper
-31352, -- Risen Drakkari Death Knight
-31338, -- Drakkari Commander
-31346) -- Flesheating Ghoul
- AND item IN (
- 37799, -- Reanimator's Cloak
- 37800, -- Aviary Guardsman's Hauberk
- 37801);-- Waistguard of the Risen Knight
+-- Insert Trash Loot Blues
+-- DELETE FROM creature_loot_template WHERE entry IN (
+-- 31339, -- Drakkari Guardian
+-- 31347, -- Ghoul Tormentor
+-- 31337, -- Drakkari Bat
+-- 31357, -- Scourge Brute
+-- 31363, -- Wretched Belcher
+-- 31336, -- Darkweb Recluse
+-- 31359, -- Scourge Reanimator
+-- 31343, -- Drakkari Scytheclaw
+-- 31355, -- Risen Drakkari Warrior
+-- 31354, -- Risen Drakkari Soulmage
+-- 31342, -- Risen Drakkari Handler
+-- 31351, -- Risen Drakkari Bat Rider
+-- 31345, -- Drakkari Shaman
+-- 31340, -- Drakkari Gutripper
+-- 31352, -- Risen Drakkari Death Knight
+-- 31338, -- Drakkari Commander
+-- 31346) -- Flesheating Ghoul
+--  AND item IN (
+--  37799, -- Reanimator's Cloak
+--  37800, -- Aviary Guardsman's Hauberk
+--  37801);-- Waistguard of the Risen Knight
 
 -- Chances from wowhead
-INSERT INTO creature_loot_template VALUES
+Replace INTO creature_loot_template VALUES
 -- Reanimator's Cloak
 (31346, 37799, 1.2, 1, 0, 1, 1), -- Flesheating Ghoul
 (31340, 37799, 1.1, 1, 0, 1, 1), -- Drakkari Gutripper
@@ -35,17 +35,19 @@ INSERT INTO creature_loot_template VALUES
 (31354, 37799, 1.1, 1, 0, 1, 1), -- Risen Drakkari Soulmage
 (31357, 37799, 1.1, 1, 0, 1, 1), -- Scourge Brute
 (31359, 37799, 1.1, 1, 0, 1, 1), -- Scourge Reanimator
-(31342, 37799, 1  , 1, 0, 1, 1), -- Risen Drakkari Handler
-(31355, 37799, 1  , 1, 0, 1, 1), -- Risen Drakkari Warrior
-(31363, 37799, 1  , 1, 0, 1, 1), -- Wretched Belcher
+(31342, 37799, 1, 1, 0, 1, 1), -- Risen Drakkari Handler
+(31355, 37799, 1, 1, 0, 1, 1), -- Risen Drakkari Warrior
+(31363, 37799, 1, 1, 0, 1, 1), -- Wretched Belcher
 (31351, 37799, 0.9, 1, 0, 1, 1), -- Risen Drakkari Bat Rider
 (31338, 37799, 0.7, 1, 0, 1, 1), -- Drakkari Commander
 (31336, 37799, 0.6, 1, 0, 1, 1), -- Darkweb Recluse
 (31339, 37799, 0.5, 1, 0, 1, 1), -- Drakkari Guardian
 (31343, 37799, 0.5, 1, 0, 1, 1), -- Drakkari Scytheclaw
-(31345, 37799, 0.5, 1, 0, 1, 1), -- Drakkari Shaman
-(31337, 37799, 0.1, 1, 0, 1, 1), -- Drakkari Bat
+(31345, 37799, 0.5, 1, 0, 1, 1); -- Drakkari Shaman
+-- (31337, 37799, 0.1, 1, 0, 1, 1); -- Drakkari Bat
+
 -- Aviary Guardsman's Hauberk
+Replace INTO creature_loot_template VALUES
 (31351, 37800, 1.5, 1, 0, 1, 1), -- Risen Drakkari Bat Rider
 (31346, 37800, 1.2, 1, 0, 1, 1), -- Flesheating Ghoul
 (31342, 37800, 1.2, 1, 0, 1, 1), -- Risen Drakkari Handler
