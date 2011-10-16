6cb7bcf8dc9f3fcc4dae01b795c28c8f9282b9d9
LORDofDOOM <tb@bbz-int.de>
2011-10-17 00:59:35 +0200

    Change SQL in VIP

--- addition/mmowning/world_vip_account_command.sql
+++ addition/mmowning/world_vip_account_command.sql
@@ -3,4 +3,5 @@ INSERT INTO `command` VALUES
 ('vip', 0, 'Syntax: .vip\r\n\r\nCheck if your account is VIP.'),
 ('vip add', 3, 'Syntax: .vip add $account $days\r\n\r\nSet or update a VIP account.');
 
-UPDATE `command` SET security = 0 WHERE name IN ('lookup tele', 'tele', 'bank');
+-- UPDATE `command` SET security = 0 WHERE name IN ('lookup tele', 'tele', 'bank');
+UPDATE `command` SET security = 0 WHERE name IN ('bank');
