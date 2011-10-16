41e7cd848daec1afd0cee6415769e85db480926e
LORDofDOOM <tb@bbz-int.de>
2011-10-16 04:40:33 +0200

    Make anticheat passive, ban cheaters for 1 hour, it can be enabled/disabled with config.
    THX to Drakeos

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1224,6 +1224,9 @@ void World::LoadConfigSettings(bool reload)
     m_int_configs[CONFIG_ANTICHEAT_DETECTIONS_ENABLED] = ConfigMgr::GetIntDefault("Anticheat.DetectionsEnabled",31);
     m_int_configs[CONFIG_ANTICHEAT_MAX_REPORTS_FOR_DAILY_REPORT] = ConfigMgr::GetIntDefault("Anticheat.MaxReportsForDailyReport",70);
 
+    // Make anticheat active, BAN cheaters for 1 hour
+   m_bool_configs[CONFIG_BAN_PLAYER] = ConfigMgr::GetBoolDefault("Anticheat.Ban", true);
+	
     // Management for channels with flag CHANNEL_DBC_FLAG_CITY_ONLY.
     m_bool_configs[CONFIG_CHANNEL_ON_CITY_ONLY_FLAG]  = ConfigMgr::GetBoolDefault("Channel.CityOnlyFlag", true);
 
