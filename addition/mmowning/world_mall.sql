9b1a2f5e7dfe3a219af146ddb0c74417823ceb53
LORDofDOOM <tb@bbz-int.de>
2011-10-17 04:32:40 +0200

    Fix SQL error

--- addition/mmowning/world_mall.sql
+++ addition/mmowning/world_mall.sql
@@ -2,4 +2,4 @@ DELETE FROM command WHERE `name`='mall';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('mall', 0, 'Syntax: .mall Teleportiert dich in die Hauptstadt deine Fraktion und belebt dich dort wieder, wenn du tot bist (Kann nicht im Kampf, Flug oder auf einem Mount verwendet werden)');
 
 DELETE FROM command WHERE `name`='dala';
-INSERT INTO `command` (`name`, `security`, `help`) VALUES ('mall', 0, 'Syntax: .dala Teleportiert dich nach Dalaran und belebt dich dort wieder, wenn du tot bist (Kann nicht im Kampf, Flug oder auf einem Mount verwendet werden)');
+INSERT INTO `command` (`name`, `security`, `help`) VALUES ('dala', 0, 'Syntax: .dala Teleportiert dich nach Dalaran und belebt dich dort wieder, wenn du tot bist (Kann nicht im Kampf, Flug oder auf einem Mount verwendet werden)');
