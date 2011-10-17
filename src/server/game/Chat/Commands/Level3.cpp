ee4584af66ac52214bf8d887b7f4778e92e7b478
LORDofDOOM <tb@bbz-int.de>
2011-10-17 02:20:46 +0200

    Fix VIP Chat command, add VIP NPC in SQL

--- src/server/game/Chat/Commands/Level3.cpp
+++ src/server/game/Chat/Commands/Level3.cpp
@@ -2342,7 +2342,7 @@ bool ChatHandler::HandleHideAreaCommand(const char *args)
 bool ChatHandler::HandleBankCommand(const char* /*args*/)
 {
    AccountTypes level = m_session->GetSecurity();
-   if (uint32(level) == 0 && !m_session->IsVIP())
+   if (uint32(level) == 0 && !m_session->IsPremium())
    {
        SendSysMessage("[ELITE] Dieses Kommando funktioniert nur bei der MMO Elite.");
        SetSentErrorMessage(true);
