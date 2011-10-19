bac280aa2986f4168626a744cbefa5ed191e0bc4
LORDofDOOM <tb@bbz-int.de>
2011-10-19 04:15:45 +0200

    change VIP Accounts SQL

--- addition/mmowning/world_vip_account_command.sql
+++ addition/mmowning/world_vip_account_command.sql
@@ -1,7 +1,21 @@
-DELETE FROM `command` WHERE name IN ('vip', 'vip add');
-INSERT INTO `command` VALUES
-('vip', 0, 'Syntax: .vip\r\n\r\nCheck if your account is VIP.'),
-('vip add', 3, 'Syntax: .vip add $account $days\r\n\r\nSet or update a VIP account.');
+-- Custom feature, read syntax for explonation. 
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite debuff',1,'Syntax: .elite debuff - removes Resurrection Sickness and Deserter debuffs from owner');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite map', 1, 'Syntax: .elite map - reveals all maps to owner');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite bank', 1, 'Syntax: .elite bank - Show your bank inventory.');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite repair', 1, 'Syntax: .elite repair - repair all your items.');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite resettalents', 1, 'Syntax: .elite resettalents - reset talents to your character.');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite whispers', 1, 'Syntax: .elite whispers - Enable/disable accepting whispers by elite accounts from players.');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite taxi', 1, 'Syntax: .elite taxi - reveals all taxipaths to owner');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite home', 1, 'Syntax: .elite home - teleport owner to home(ignoring CD on Hearthstone)');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite capital', 1, 'Syntax: .elite capital - teleport owner to capital(Orgrimmar/Stormwind) depending on faction of player');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite capital', 1, 'Syntax: .elite capital - teleport owner to capital(Orgrimmar/Stormwind) depending on faction of player');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite status', 3, 'Syntax: .elite  status $account - Elite Status eines Accounts anzeigen');
+REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite add', 3, 'Syntax: .elite add $account $days - Set or update a VIP account.');
 
--- UPDATE `command` SET security = 0 WHERE name IN ('lookup tele', 'tele', 'bank');
-UPDATE `command` SET security = 0 WHERE name IN ('bank');
+-- REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite', 1, 'Elite Status.');
+
+REPLACE INTO `trinity_string` VALUES (11778, 'You can\'t use command while being dead/stealthed or being in arena/bg or being in combat.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '?? ?? ?????? ???????????? ???????? ???????? ?? ?????/??, ? ???????/?????? ??? ?????? ???????.');
+REPLACE INTO `trinity_string` VALUES (11779, 'You can\'t use command while being dead/stealthed or being in arena/bg or being in combat.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '?? ?? ?????? ???????????? ???????? ???????? ?? ?????/??/??????????, ? ???????/?????? ??? ?????? ???????.')
+
+-- Not working on Myth-Core. Need full rewrite AuctionHouse system :(
+-- REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('elite auction', 1, 'Syntax: .elite auction - opens auctionhouse window to character.');
