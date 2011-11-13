87aef829713b2e2371124bd8ab69ed83f5dbb18d
LORDofDOOM <LORDofDOOM@xxx.xxx>
2011-11-13 21:52:38 +0100

    Override GM_Commands for GM PlayerChars (LV1)

---
+++ sql/updates/mmo_updates/world_custom_gmcommands.sql
@@ -0,0 +1,211 @@
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'whispers';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'tele name';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'tele group';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'tele';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'summon';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'send mail';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'saveall';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'whispers';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'recall';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'npc yell';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'npc whisper';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'npc textemote';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'npc say';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'npc addformation';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'npc';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'notify';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'namego';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'nameannounce';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'mute';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'modify';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'lookup area';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'learn all_default';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'groupsummon';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'gps';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'goname';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go zonexy';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go xyz';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go xy';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go trigger';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go ticket';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go taxinode';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go object';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go grid';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go graveyard';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go creature';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'gmnameannounce';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'gm visible';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'gm chat';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'gm';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'debug play sound';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'debug play movie';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'debug play cinematic';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'debug areatriggers';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'debug';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'appear';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'announce';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'account addon';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'go';
+-- Modifiy
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify tp';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify swim';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify spell';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'modify speed';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'modify scale';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify runicpower';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify rage';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify mount';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify money';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify mana';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify hp';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify honor';
+-- Original 1
+UPDATE command SET security = '2' WHERE name = 'modify fly';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify faction';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify energy';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify drunk';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify bwalk';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify bit';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify aspeed';
+-- Original 1
+UPDATE command SET security = '3' WHERE name = 'modify arena';
+
+
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'revive';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'respawn';
+-- Original 3
+UPDATE command SET security = '5' WHERE name = 'server shutdown';
+-- Original 3
+UPDATE command SET security = '5' WHERE name = 'server shutdown cancel';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'ticket delete';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'unaura';
+-- Original 4
+UPDATE command SET security = '3' WHERE name = 'achievement';
+-- Original 4
+UPDATE command SET security = '3' WHERE name = 'achievement add';
+-- Original 3
+UPDATE command SET security = '5' WHERE name = 'reset all';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup creature';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup faction';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup item';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup itemset';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup map';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup object';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup quest';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup skill';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup spell';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'lookup taxinode';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'quest';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'quest add';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'quest complete';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'quest remove';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'gm fly';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'instance unbind';
+-- Original 3
+UPDATE command SET security = '2' WHERE name = 'account onlinelist';
+
+-- AntiCheat for GM PlayerChars
+-- Original 1
+UPDATE command SET security = '1' WHERE name = 'anticheat';
+-- Original 2
+UPDATE command SET security = '1' WHERE name = 'anticheat delete';
+-- Original 2
+UPDATE command SET security = '1' WHERE name = 'anticheat global';
+-- Original 2
+UPDATE command SET security = '1' WHERE name = 'anticheat handle';
+-- Original 2
+UPDATE command SET security = '1' WHERE name = 'anticheat player';
