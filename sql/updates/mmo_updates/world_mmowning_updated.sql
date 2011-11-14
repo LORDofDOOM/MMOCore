7890d1f888690f3c179beedcf3022625891b825b
LORDofDOOM <LORDofDOOM@xxx.xxx>
2011-11-14 01:27:20 +0100

    Add SQL

---
+++ sql/updates/mmo_updates/world_mmowning_updated.sql
@@ -0,0 +1,58 @@
+-- Missing Script Names
+
+REPLACE INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction_A`, `faction_H`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `Health_mod`, `Mana_mod`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `equipment_id`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) VALUES
+(33142, 0, 0, 0, 0, 0, 28829, 0, 0, 0, 'Leviathan Defense Turret', '', '', 0, 80, 80, 2, 2101, 2101, 0, 1, 1, 1, 0, 422, 586, 0, 642, 1, 2000, 0, 1, 33554432, 8, 0, 0, 0, 0, 0, 345, 509, 103, 9, 12288, 0, 0, 0, 0, 0, 0, 0, 0, 0, 62402, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 5.07937, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 'npc_flame_leviathan_defense_turret', 12340),
+(33143, 0, 0, 0, 0, 0, 1126, 11686, 28469, 0, 'Overload Control Device', '', '', 0, 80, 80, 0, 35, 35, 16777216, 1.2, 1, 1, 0, 422, 586, 0, 642, 1, 2000, 0, 1, 33554432, 8, 0, 0, 0, 0, 0, 345, 509, 103, 9, 1049600, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 'npc_flame_leviathan_overload_device', 12340),
+(33114, 0, 0, 0, 0, 0, 11686, 24914, 0, 0, 'Flame Leviathan Seat', '', 'vehichleCursor', 0, 80, 80, 2, 2110, 2110, 16777216, 1, 1.14286, 1, 0, 0, 0, 0, 0, 1, 2000, 0, 4, 33570816, 8, 0, 0, 0, 0, 0, 0, 0, 0, 9, 1049632, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 341, 0, 0, '', 0, 3, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 'npc_flame_leviathan_seat', 12340),
+(33139, 34111, 0, 0, 0, 0, 28883, 0, 0, 0, 'Flame Leviathan Turret', '', 'Gunner', 0, 80, 80, 0, 1965, 1965, 16777216, 1.2, 1.14286, 1, 3, 0, 0, 0, 0, 35, 2000, 0, 4, 33571584, 8, 0, 0, 0, 0, 0, 0, 0, 0, 9, 786540, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 935.979, 1, 1, 0, 0, 0, 0, 0, 0, 0, 121, 1, 0, 0, 0, 'npc_flame_leviathan_defense_cannon', 12340),
+(33189, 0, 0, 0, 0, 0, 28783, 28476, 0, 0, 'Liquid Pyrite', '', '', 0, 80, 80, 2, 2059, 2059, 0, 1.2, 1, 1, 0, 422, 586, 0, 642, 1, 2000, 0, 1, 33280, 8, 0, 0, 0, 0, 0, 345, 509, 103, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0.000476, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 'npc_liquid_pyrite', 12340),
+(33387, 34277, 0, 0, 0, 0, 14214, 19030, 16870, 0, 'Writhing Lasher', '', '', 0, 80, 80, 2, 16, 16, 0, 1, 2.57143, 1, 1, 422, 586, 0, 642, 7.5, 2000, 0, 1, 0, 8, 0, 0, 0, 0, 0, 345, 509, 103, 4, 524360, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 5.1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 210, 1, 0, 0, 0, 'npc_freyas_ward_summon', 12340),
+(33060, 0, 0, 0, 0, 0, 28650, 0, 0, 0, 'Salvaged Siege Engine', '', 'vehichleCursor', 0, 80, 80, 2, 2105, 2105, 0, 1.25, 1, 1, 0, 0, 0, 0, 0, 1, 2000, 0, 4, 16384, 8, 0, 0, 0, 0, 0, 0, 0, 0, 9, 262184, 0, 0, 0, 0, 0, 0, 0, 0, 0, 62345, 62522, 62346, 0, 0, 0, 0, 0, 0, 336, 0, 0, '', 0, 3, 90, 1, 1, 0, 0, 0, 0, 0, 0, 0, 113, 1, 0, 377831290, 0, 'npc_leviathan_player_vehicle', 12340),
+(33062, 34045, 0, 0, 0, 0, 25870, 25871, 0, 0, 'Salvaged Chopper', '', 'vehichleCursor', 0, 80, 80, 2, 2105, 2105, 16777216, 2, 2, 1, 0, 0, 0, 0, 0, 1, 2000, 0, 4, 16384, 8, 0, 0, 0, 0, 0, 0, 0, 0, 9, 262176, 0, 0, 0, 0, 0, 0, 0, 0, 0, 62974, 62286, 62299, 64660, 0, 0, 0, 0, 0, 335, 0, 0, '', 0, 3, 40, 1, 1, 0, 0, 0, 0, 0, 0, 0, 180, 1, 0, 377831290, 0, 'npc_leviathan_player_vehicle', 12340),
+(33109, 0, 0, 0, 0, 0, 27658, 0, 0, 0, 'Salvaged Demolisher', '', 'vehichleCursor', 0, 80, 80, 2, 2105, 2105, 0, 1.2, 1, 1, 0, 0, 0, 0, 0, 1, 2000, 0, 4, 16384, 8, 0, 0, 0, 0, 0, 0, 0, 0, 9, 393256, 0, 0, 0, 0, 0, 0, 0, 0, 0, 62306, 62490, 62308, 62324, 0, 0, 0, 0, 0, 338, 0, 0, '', 0, 3, 50, 0.5, 1, 0, 0, 0, 0, 0, 0, 0, 113, 1, 0, 377831290, 0, 'npc_leviathan_player_vehicle', 12340),
+(33488, 33789, 0, 0, 0, 0, 28639, 0, 0, 0, 'Saronite Vapors', '', '', 0, 80, 80, 2, 16, 16, 0, 1, 1.14286, 1, 0, 422, 586, 0, 642, 1, 2000, 0, 1, 0, 8, 0, 0, 0, 0, 0, 345, 509, 103, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 'mob_saronit_vapor', 12340),
+(33524, 34152, 0, 0, 0, 0, 28992, 0, 0, 0, 'Saronite Animus', '', '', 0, 76, 76, 2, 16, 16, 0, 1, 1.14286, 1, 3, 356, 503, 0, 432, 35, 2000, 0, 1, 0, 8, 0, 0, 0, 0, 0, 305, 452, 74, 10, 108, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 113, 1, 1, 0, 0, 0, 0, 0, 0, 0, 144, 1, 0, 0, 0, 'mob_saronit_animus', 12340),
+(33229, 0, 0, 0, 0, 0, 28048, 0, 0, 0, 'Melee Target', '', '', 0, 80, 80, 0, 634, 634, 0, 1, 1, 1, 0, 422, 586, 0, 642, 1, 1000, 0, 1, 131076, 132, 0, 0, 0, 0, 0, 345, 509, 103, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1.87196, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 262144, 'npc_training_dummy_argent', 12340),
+(33243, 0, 0, 0, 0, 0, 28938, 28048, 0, 0, 'Ranged Target', '', '', 0, 80, 80, 2, 634, 634, 0, 1, 1.38571, 1, 0, 422, 586, 0, 642, 1, 1000, 0, 1, 4, 8, 0, 0, 0, 0, 0, 345, 509, 103, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1.5873, 1, 0, 0, 0, 0, 0, 0, 0, 0, 140, 1, 0, 0, 0, 'npc_training_dummy_argent', 12340),
+(33272, 0, 0, 0, 0, 0, 29075, 0, 0, 0, 'Charge Target', '', '', 0, 80, 80, 0, 634, 634, 0, 1, 1.38571, 1, 0, 422, 586, 0, 642, 1, 1000, 0, 1, 4, 136, 0, 0, 0, 0, 0, 345, 509, 103, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1.87196, 1, 0, 0, 0, 0, 0, 0, 0, 0, 140, 1, 0, 0, 0, 'npc_training_dummy_argent', 12340);
+
+REPLACE INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES	
+(5369, 'at_RX_214_repair_o_matic_station'),
+(5423, 'at_RX_214_repair_o_matic_station');
+	
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES	
+(62359, 'spell_anti_air_rocket'),
+(64979, 'spell_anti_air_rocket'),	
+(62374, 'spell_pursued'),	
+(65044, 'spell_flame_leviathan_flames'),
+(65045, 'spell_flame_leviathan_flames'),	
+(62324, 'spell_throw_passenger'),
+(62907, 'spell_freyas_ward_summon'),	
+(63847, 'spell_flame_leviathan_flame_vents'),	
+(64677, 'spell_shield_generator'),	
+(65121, 'spell_xt002_searing_light'),	
+(63018, 'spell_xt002_searing_light'),	
+(63024, 'spell_xt002_gravity_bomb'),	
+(64234, 'spell_xt002_gravity_bomb'),
+(62692, 'spell_general_vezax_aura_of_despair_aura'),
+(63276, 'spell_general_vezax_mark_of_the_faceless_aura'),
+(63278, 'spell_general_vezax_mark_of_the_faceless_drain'),
+(61911, 'spell_steelbreaker_static_disruption'),	
+(61900, 'spell_steelbreaker_electrical_charge');	
+	
+	
+REPLACE INTO `achievement_criteria_data` (`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES	
+(10451, 11, 0, 0, 'achievement_i_love_the_smell_of_saronite_in_the_morning'),
+(10462, 11, 0, 0, 'achievement_i_love_the_smell_of_saronite_in_the_morning'),	
+(10087, 11, 0, 0, 'achievement_i_choose_you'),
+(10086, 11, 0, 0, 'achievement_i_choose_you'),
+(10085, 11, 0, 0, 'achievement_i_choose_you'),
+(10084, 11, 0, 0, 'achievement_i_choose_you'),
+(10083, 11, 0, 0, 'achievement_i_choose_you'),
+(10082, 11, 0, 0, 'achievement_i_choose_you'),
+(10419, 11, 0, 0, 'achievement_but_i_am_on_your_side'),
+(10418, 11, 0, 0, 'achievement_but_i_am_on_your_side'),
+(10088, 11, 0, 0, 'achievement_but_i_am_on_your_side'),
+(10089, 11, 0, 0, 'achievement_but_i_am_on_your_side'),
+(10420, 11, 0, 0, 'achievement_but_i_am_on_your_side'),
+(10421, 11, 0, 0, 'achievement_but_i_am_on_your_side');
