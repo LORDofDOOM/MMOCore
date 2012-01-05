ae020ee23a79f5afed7c5f93b9d5108cda233a9d
LORDofDOOM <xxx@xxx.xxx>
2012-01-05 03:21:54 +0100

    ICC: Fix Sister Svalna falling under textures

--- src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel.cpp
@@ -885,6 +885,10 @@ class boss_sister_svalna : public CreatureScript
                             Talk(SAY_SVALNA_EVENT_START);
                             break;
                         case EVENT_SVALNA_RESURRECT:
+                            me->setActive(false);
+                            me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE | UNIT_FLAG_PASSIVE);
+                            me->SetFlying(false);
+                            me->SetPosition(4356.33f, 2484.21f, 358.43f, 1.54f);
                             Talk(SAY_SVALNA_RESURRECT_CAPTAINS);
                             me->CastSpell(me, SPELL_REVIVE_CHAMPION, false);
                             break;
