a78c0e3465ac662e009b8cbe2edca4f4ab8247de
LORDofDOOM <tb@bbz-int.de>
2012-03-08 22:59:45 +0100

    [SQL] Add some commands for test players

--- sql/updates/world/TESTSERVER_ONLY/world_custom_TESTSERVER_ONLY_command.sql
+++ sql/updates/world/TESTSERVER_ONLY/world_custom_TESTSERVER_ONLY_command.sql
@@ -1,2 +1,9 @@
 -- Allow players to unbind instance
 UPDATE command SET security = '0' WHERE name = 'instance unbind';
+-- Allow players modify speed
+UPDATE command SET security = '0' WHERE name = 'modify aspeed';
+UPDATE command SET security = '0' WHERE name = 'modify speed';
+UPDATE command SET security = '0' WHERE name = 'repairitems';
+UPDATE command SET security = '0' WHERE name = 'revive';
+UPDATE command SET security = '0' WHERE name = 'maxskill';
+UPDATE command SET security = '0' WHERE name = 'additem';
