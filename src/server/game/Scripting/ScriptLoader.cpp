1b351b9e69454e945ac6c795e9cfac8455f9b12c
LORDofDOOM <tb@bbz-int.de>
2012-03-17 19:19:05 +0100

    [Core] Revert DuelReset -> FC Script is working better

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -110,7 +110,7 @@ void AddSC_titles_commandscript();
 void AddSC_wp_commandscript();
 void AddSC_vip_commandscript();
 void AddSC_gps_commandscript();
-void AddSC_Reset();
+//void AddSC_Reset();
 void AddSC_spectate_commandscript();
 
 #ifdef SCRIPTS
@@ -726,7 +726,7 @@ void AddCommandScripts()
     AddSC_wp_commandscript();
     AddSC_vip_commandscript();
     AddSC_gps_commandscript();
-	AddSC_Reset();
+	//AddSC_Reset();
     AddSC_spectate_commandscript();	
 }
 
