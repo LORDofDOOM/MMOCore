c271a282fddb0a78a19b0492c3f07917212013a3
LORDofDOOM <tb@bbz-int.de>
2012-06-24 21:14:30 +0200

    [SQL] Change Battlegrounds

--- sql/updates/mmo_updates_world/optional/world_mmowning_customize_bg.sql
+++ sql/updates/mmo_updates_world/optional/world_mmowning_customize_bg.sql
@@ -1,5 +1,5 @@
 -- Alterac Valley Original 20
-UPDATE battleground_template SET MinPlayersPerTeam 	 = '10' WHERE id = 1;
+UPDATE battleground_template SET MinPlayersPerTeam 	 = '8' WHERE id = 1;
 -- Warsong Gulch Original 5
 UPDATE battleground_template SET MinPlayersPerTeam 	 = '3' WHERE id = 2;
 -- Arathi Basin Original 8
@@ -7,6 +7,6 @@ UPDATE battleground_template SET MinPlayersPerTeam 	 = '4' WHERE id = 3;
 -- Eye of The Storm Original 8
 UPDATE battleground_template SET MinPlayersPerTeam 	 = '4' WHERE id = 7;
 -- Isle of Conquest Original 20
-UPDATE battleground_template SET MinPlayersPerTeam 	 = '10' WHERE id = 30;
+UPDATE battleground_template SET MinPlayersPerTeam 	 = '8' WHERE id = 30;
 -- Random battleground Original 20
 UPDATE battleground_template SET MinPlayersPerTeam 	 = '5' WHERE id = 32;
